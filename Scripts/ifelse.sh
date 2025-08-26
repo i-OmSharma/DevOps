edf5c46711e88336145dd644a8850a301987dd5d
i-OmSharma <sharmaom1201@gmail.com>
2025-08-27 00:30:20 +0530

    if else

---
+++ Scripts/ifelse.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# if else
+# if [expression]
+# then
+# 	//body
+# else
+# 	//body
+# fi
+#
+#
+
+a=4
+b=1
+
+if [ $a > $b ]
+then
+	echo "a is greater then b"
+else 
+	echo "b is greter then a"
+fi
