4d1da4e0ee635c3f462d5b96d5fa0d097ea99f48
i-OmSharma <sharmaom1201@gmail.com>
2025-12-15 21:43:13 +0530

    output block

--- Terraform /ec2/ec2.tf
+++ Terraform /ec2/ec2.tf
@@ -80,10 +80,3 @@ resource "aws_instance" "my_instance" {
 }
 
 
-output "ec2_public_ip" {
-  value = aws_instance.my_instance.public_ip
-}
-
-output "ec2_public_dns" {
-  value = aws_instance.my_instance.public_dns
-}
